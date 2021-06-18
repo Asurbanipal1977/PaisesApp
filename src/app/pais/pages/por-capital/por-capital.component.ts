4302ec170466b8ede8ee027ab463ab163e2416c5
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2021-06-18 15:53:16 +0200

    Sección 8

---
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -0,0 +1,44 @@
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-capital',
+  templateUrl: './por-capital.component.html',
+  styles: [
+  ]
+})
+export class PorCapitalComponent implements OnInit {
+
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor( private paisService: PaisService) { }
+
+  ngOnInit(): void {
+  }
+
+  buscar( termino: string){
+    this.hayError = false;
+    this.termino = termino;
+    this.paisService.buscarCapital(termino).subscribe( (paises) => 
+        {
+          this.paises=paises;
+        }, (err) => {
+          this.hayError = true;
+          this.paises = [];
+          console.log (err);
+        }
+      );
+  }
+
+  sugerencias (termino: string)
+  {
+    this.hayError = false;
+  }
+
+}
+
+
+
